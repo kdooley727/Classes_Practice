fa738d8949fa1d67fb48a9b42b7d402f71ddae67
kdooley727 <104645526+kdooley727@users.noreply.github.com>
2022-05-26 19:58:35 -0700

    Add files via upload

---
+++ Person_Class_Practice_Dooley/ConsoleApp1/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Person
+
+        //Person class Kyle Dooley 05/26/2022
+    {
+        public Person() { }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        public void Introduction()
+        {
+            Console.WriteLine("Hello, my name is " + FirstName + " " + LastName);
+        }
+
+        public void DisplayContactInfo()
+        {
+            Console.WriteLine("Contact Info Address: {0}, Email: {1}", Address, Email);
+        }
+    }
+}
---
+++ Person_Class_Practice_Dooley/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, There!");
+
+            //Classes Exercise instantiation of an object Kyle Dooley 05/26/2022
+            Person person = new Person();
+            person.FirstName = "Robert";
+            person.LastName = "Pickle";
+            person.Address = "66 Sandstone Dr";
+            person.Email = "rpickle@domain.com";
+            person.Introduction();
+            person.DisplayContactInfo();
+
+            Console.WriteLine();
+
+            var person2 = new Person();
+            person2.FirstName = "Lex";
+            person2.LastName = "Money";
+            person2.Address = "187 Birchwood Cir";
+            person2.Email = "lmoney@domain.com";
+            person2.Introduction();
+            person2.DisplayContactInfo();
+
+            Console.WriteLine();
+
+            var person3 = new Person();
+            person3.FirstName = "Finley";
+            person3.LastName = "Teagan";
+            person3.Address = "161 Buttercup Dr";
+            person3.Email = "fteagan@domain.com";
+            person3.Introduction();
+            person3.DisplayContactInfo();
+            
+            Console.ReadLine();
+        }
+
+    }
+}
